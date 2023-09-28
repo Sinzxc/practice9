3141a7697bb2b4347b5faabd213e1c6eb8379777
Sinzxc <nik.orig@mail.ru>
2023-09-28 16:41:08 +0300

    complete

---
+++ app/src/main/java/ru/mggtk/practice9/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.mggtk.practice9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val text: TextView = findViewById(R.id.textView3)
+        text.setText("Copyright (C) 2021")
+    }
+}
